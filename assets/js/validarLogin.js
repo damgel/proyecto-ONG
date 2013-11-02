2319ade4742793a205b5bcee9a94d65049414455
damgel <damgel@outlook.com>
2013-11-02 00:19:38 -0600

    reglas js de validacion agregadas para el login

--- assets/js/validarLogin.js
+++ assets/js/validarLogin.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
- 
+
 // codigo personalizado para mis validaciones
     $('#frm-login').validate({
         rules: {
-            email: 
-            {
-                required: true,
-                email: true
-                
-            },
-            passwordm:
+            JVPO:
                     {
-                minlength: 8,
-                required:true    
+                        required: true,
+                        minlenght: 6
+                    },
+            password_doc:
+                    {
+                        required: true,
+                        minlenght: 6
                     }
         }
     });
