1c8aa3f45a49d9e8bb30ce57a1fb92ce00600632
damgel <damgel@outlook.com>
2013-11-04 13:35:20 -0600

    clase con funciones utiles: getEdad

---
+++ clases/utils.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of utils
+ *
+ * @author daMgeL
+ */
+class utils {
+
+    public static function getEdad($fechanacimiento) {
+        list($ano, $mes, $dia) = explode("-", $fechanacimiento);
+        $ano_diferencia = date("Y") - $ano;
+        $mes_diferencia = date("m") - $mes;
+        $dia_diferencia = date("d") - $dia;
+        if ($dia_diferencia < 0 && $mes_diferencia <= 0)
+            $ano_diferencia--;
+        return $ano_diferencia . "anios";
+    }
+
+}
+
+?>
