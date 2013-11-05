277581238ad792928043513e47cb9677ce12cb5c
Ramiro Diaz <ramirinsv@gmail.com>
2013-11-04 21:46:23 -0600

    script para tabla contactenos

---
+++ db/contactenos.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-11-2013 a las 04:43:16
+-- Versión del servidor: 5.6.11
+-- Versión de PHP: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `cocides`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contactenos`
+--
+
+CREATE TABLE IF NOT EXISTS `contactenos` (
+  `nombre_contact` varchar(30) NOT NULL,
+  `telefono_contact` varchar(8) NOT NULL,
+  `correo_contact` varchar(30) NOT NULL,
+  `motivo_contact` varchar(30) NOT NULL,
+  `mensaje_contact` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
